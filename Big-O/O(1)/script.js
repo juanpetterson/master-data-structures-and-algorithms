9501dc18109ee60c689fbd1c97b1912e4a9013fa
juanpetterson <juan.petterson@gmail.com>
2020-05-05 20:24:50 -0300

    Overview about Constant time - O(1)

---
+++ Big-O/O(1)/script.js
@@ -0,0 +1,15 @@
+const boxes = [1, 2, 3, 4, 5, 6];
+
+function compressFirstBox(boxes) {
+  console.log(boxes[0]);
+}
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]);
+  console.log(boxes[1]);
+}
+
+compressFirstBox(boxes);
+logFirstTwoBoxes(boxes);
+
+// Constant Time - O(1)
