4cee0d6506287fec56e3ce39acb032b2ed3eef1b
juanpetterson <juan.petterson@gmail.com>
2020-05-05 20:15:33 -0300

    Understanding linear time - O(n)

---
+++ Big-O/O(n)/script.js
@@ -0,0 +1,26 @@
+const nemo = ['nemo'];
+const everyone = [
+  'dory',
+  'bruce',
+  'marlin',
+  'nemo',
+  'gill',
+  'bloat',
+  'nigel',
+  'squirt',
+  'darla',
+  'hank',
+];
+const large = new Array(1000).fill('nemo');
+
+function findNemo1(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+}
+
+findNemo1(large);
+
+// O(n) --> Linear Time
