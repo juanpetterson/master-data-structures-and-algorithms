f4a6dfc70776e978d2e761a2742fde6d10bcdd70
juanpetterson <juan.petterson@gmail.com>
2020-05-05 21:31:43 -0300

    Understanding n^2 big o

---
+++ Big-O/O(n^2)/script.js
@@ -0,0 +1,27 @@
+// const boxes = [1, 2, 3, 4, 5];
+
+// function logAllPairsOfArray(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       console.log(i, j);
+//     }
+//   }
+// }
+
+// logAllPairsOfArray(boxes);
+
+function printAllNumbersThenAllPairSums(numbers) {
+  console.log('these are the numbers:');
+  numbers.forEach(function (number) {
+    console.log(number);
+  });
+
+  console.log('and these are their sums:');
+  numbers.forEach(function (firstNumber) {
+    numbers.forEach(function (secondNumber) {
+      console.log(firstNumber + secondNumber);
+    });
+  });
+}
+
+printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]);
