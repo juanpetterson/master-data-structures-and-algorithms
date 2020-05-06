930e6a5bac7dd2b710c1426535725b91b59915da
juanpetterson <juan.petterson@gmail.com>
2020-05-05 23:05:50 -0300

    Twitter feature big O examples

---
+++ Exercise Twitter/script.js
@@ -0,0 +1,13 @@
+// Find 1st, Find Nth...
+const array = [
+  { tweet: 'hi', date: 2012 },
+  { tweet: 'my', date: 2014 },
+  { tweet: 'tweet', date: 2016 },
+];
+array[0]; // O(1)
+array[array.length - 1]; // O(1)
+
+// Compare the dates of the tweets O(n^2)
+
+'hellowordhowlongisthsi'.length; // length is a property, not a function, so we just need to
+// access the value, then, the big O is O(1)
