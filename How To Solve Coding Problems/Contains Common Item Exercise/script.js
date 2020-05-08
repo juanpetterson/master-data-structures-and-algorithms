5172eec1b27382e719aed2dddd455a69345fa321
juanpetterson <juan.petterson@gmail.com>
2020-05-07 21:12:12 -0300

    Working with solving problems with diffent approachs

---
+++ How To Solve Coding Problems/Contains Common Item Exercise/script.js
@@ -0,0 +1,73 @@
+// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
+//For Example:
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
+//should return false.
+//-----------
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+//should return true.
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'x'];
+
+function containsCommonItem(array1, array2) {
+  let found = false;
+  array1.forEach((item) => {
+    array2.forEach((item2) => {
+      if (item === item2) {
+        found = true;
+        return;
+      }
+    });
+  });
+
+  return found;
+}
+
+// O(a*b) Time Complexity
+// O(1) Space Complexity
+
+console.log('containsCommonItem: ' + containsCommonItem(array1, array2));
+
+// array1 ==> obj {
+//   a: true,
+//   b: true,
+//   c: true,
+//   x: true,
+// };
+
+// array2[index] === obj.properties
+
+function containsCommonItem2(arr1, arr2) {
+  // loop through first array and create objct where properties equal to the items in the array
+  let map = {};
+  arr1.forEach((item, index) => {
+    if (!map[index]) {
+      map[item] = true;
+    }
+  });
+
+  // loop through second array and check if item in second array exists on create obejct
+  let found = false;
+  arr2.forEach((item2) => {
+    if (map[item2]) {
+      found = true;
+      return;
+    }
+  });
+
+  return found;
+}
+
+// O(a+b) Time Complexity
+// O(a) Space Complexity
+
+console.log('containsCommonItem2: ' + containsCommonItem2(array1, array2));
+
+function containsCommonItem3(arr1, arr2) {
+  return arr1.some((item) => arr2.includes(item));
+}
+
+console.log('containsCommonItem3: ' + containsCommonItem3(array1, array2));
---
+++ How To Solve Coding Problems/Google Interview Question/script.js
@@ -0,0 +1,40 @@
+// Naive
+function hasPairWithSum(arr, sum) {
+  let len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] === sum) return true;
+    }
+  }
+
+  return false;
+}
+
+// Better
+// function hasPairWithSum2(arr, sum) {
+//   const mySet = new Set();
+//   const len = arr.length;
+//   for (let i = 0; i < len; i++) {
+//     if (mySet.has(arr[i])) {
+//       return true;
+//     }
+//     mySet.add(sum - arr[i]);
+//   }
+//   return false;
+// }
+
+function hasPairWithSum2(arr, sum) {
+  const mySet = new Set();
+  let found = false;
+  arr.forEach((item) => {
+    if (mySet.has(item)) {
+      found = true;
+      return;
+    }
+
+    mySet.add(sum - item);
+  });
+  return found;
+}
+
+console.log(hasPairWithSum2([6, 4, 3, 2, 1, 7], 9));
